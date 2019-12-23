62b944131791c8c176a967387f5af395e77c59e4
dengxinixn <939149733@qq.com>
2019-12-23 15:39:30 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,57 +1,54 @@
 module.exports = {
-  title: 'icccyyy Blog',
-  base: '/icyblog/',
-  description: '吾欲追晚否？ 曰:先追了在说~',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-  ],
+  title: "icccyyy Blog",
+  base: "/icyblog/",
+  description: "吾欲追晚否？ 曰:先追了在说~",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   themeConfig: {
     sidebarDepth: 2,
     search: false,
     // lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
     nav: [
-      { text: '首页', link: '/' },
-      { text: 'JavaScript系列', link: '/javascript/' }, // 内部链接 以docs为根目录
-      { text: 'Css系列', link: '/css/' },
-      { text: '工具箱', link: '/worktool/' },
+      { text: "首页", link: "/" },
+      { text: "JavaScript系列", link: "/javascript/" }, // 内部链接 以docs为根目录
+      { text: "Css系列", link: "/css/" },
+      { text: "工具箱", link: "/worktool/" }
     ],
     sidebar: {
-      '/javascript/': [
+      "/javascript/": [
         {
-          title: 'JavaScript系列',
+          title: "JavaScript系列",
           collapsable: false,
           children: [
-            ['/javascript/', '原型与原型链'],
-            '/javascript/变量对象',
-            '/javascript/执行上下文',
-            '/javascript/作用域与作用域链',
-            '/javascript/mapParseInt',
-            '/javascript/mvvm',
-            '/javascript/promise',
-            '/javascript/this指向问题',
-            '/javascript/10W条数据优化'
+            ["/javascript/", "原型与原型链"],
+            "/javascript/变量对象",
+            "/javascript/执行上下文",
+            "/javascript/作用域与作用域链",
+            "/javascript/mapParseInt",
+            "/javascript/mvvm",
+            "/javascript/promise",
+            "/javascript/this指向问题",
+            "/javascript/10W条数据优化"
           ]
         }
       ],
-      '/css/': [
+      "/css/": [
         {
-          title: 'Css系列',
+          title: "Css系列",
           collapsable: false,
-          children: [
-            ['/css/', 'CSS权重'],
-          ]
+          children: [["/css/", "CSS权重"]]
         }
       ],
-      '/worktool/': [
+      "/worktool/": [
         {
-          title: '工具箱',
+          title: "工具箱",
           collapsable: false,
           children: [
-            ['/worktool/', '日常正则'],
-            '/worktool/React拖拽排序',
+            ["/worktool/", "日常正则"],
+            "/worktool/React拖拽排序",
+            "/worktool/其他常用"
           ]
         }
       ]
     }
   }
-}
+};
